04c7237740b16dd65a223c923687c8132a9a07a3
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-02 13:05:20 +0000

    Day02 tidyup

--- aoc2022/Puzzles/Day02.cs
+++ aoc2022/Puzzles/Day02.cs
@@ -4,52 +4,38 @@
     {
         public override int Day => 2;
         public override string Name => "Rock Paper Scissors";
-        protected override object RunPart1() => Calc(_outcomes);
-        protected override object RunPart2() => Calc(_outcomes2);
+        protected override object RunPart1() => Calc(true);
+        protected override object RunPart2() => Calc(false);
 
-        private static Dictionary<string, int> _outcomes = new Dictionary<string, int>
+        private const int Rock = 1;
+        private const int Paper = 2;
+        private const int Sciossors = 3;
+        private const int Loss = 0;
+        private const int Draw = 3;
+        private const int Win = 6;
+
+        private static readonly Dictionary<string, (int o1, int o2)> _outcomes = new()
         {
-            {"AX", 4 }, //R R = R(1) + Draw(3)
-            {"AY", 8 }, //R P = P(2) + Win(6)
-            {"AZ", 3 }, //R S = S(3) + Loss(0) 
-            {"BX", 1 }, //P R = R(1) + Loss(0)
-            {"BY", 5 }, //P P = P(2) + Draw(3)
-            {"BZ", 9 }, //P S = S(3) + Win(6)
-            {"CX", 7 }, //S R = R(1) + Win(6)
-            {"CY", 2 }, //S P = P(2) + Loss(0)
-            {"CZ", 6 }, //S S = S(3) + Draw(3)
+            {"AX", (Rock+Draw,      Sciossors+Loss)},
+            {"AY", (Paper+Win,      Rock+Draw)},
+            {"AZ", (Sciossors+Loss, Paper+Win)},
+            {"BX", (Rock+Loss,      Rock+Loss)},
+            {"BY", (Paper+Draw,     Paper+Draw)},
+            {"BZ", (Sciossors+Win,  Sciossors+Win)},
+            {"CX", (Rock+Win,       Paper+Loss)},
+            {"CY", (Paper+Loss,     Sciossors+Draw)},
+            {"CZ", (Sciossors+Draw, Rock+Win)}
         };
 
-        private static Dictionary<string, int> _outcomes2 = new Dictionary<string, int>
-        {
-            {"AX", 3 }, //R S = S(3) + Loss(0) 
-            {"AY", 4 }, //R R = R(1) + Draw(3)
-            {"AZ", 8 }, //R P = P(2) + Win(6)
-            {"BX", 1 }, //P R = R(1) + Loss(0)
-            {"BY", 5 }, //P P = P(2) + Draw(3)
-            {"BZ", 9 }, //P S = S(3) + Win(6)
-            {"CX", 2 }, //S P = P(2) + Loss(0) 
-            {"CY", 6 }, //S S = S(3) + Draw(3)
-            {"CZ", 7 }, //S R = R(1) +  Win(6)
-        };
+        public Day02() : base("Inputs/Day02.txt") { }
 
-        public Day02() : base("Inputs/Day02.txt")
-        {
-
-        }
-
-        
-
-        private object Calc(Dictionary<string, int> o)
+        private object Calc(bool first)
         {
             var score = 0;
-            foreach(var row in PuzzleInput)
-            {
-                var s = row[0].ToString() + row[2].ToString();
-                score += o[s];
-            }
+            foreach (var row in PuzzleInput.Select(x => new string(new[] { x[0], x[2] })))
+                score += first ? _outcomes[row].o1 : _outcomes[row].o2;
+
             return score;
         }
-            
     }
 }
