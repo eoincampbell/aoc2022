b31f273898915542cedd64191dc37299edd37aec
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-05 08:05:11 +0000

    Day 04 Cleanup

--- aoc2022/Puzzles/Day04.cs
+++ aoc2022/Puzzles/Day04.cs
@@ -4,39 +4,27 @@
     {
         public override int Day => 4;
         public override string Name => "Camp Cleanup";
-        protected override object RunPart1() => Calc(true);
-        protected override object RunPart2() => Calc(false);
-
-        public Day04() : base("Inputs/Day04.txt") { }
-
-        private object Calc(bool part1)
+        protected override object RunPart1() => _countA;
+        protected override object RunPart2() => _countB;
+        private int _countA;
+        private int _countB;
+        public Day04() : base("Inputs/Day04.txt") => Calc();
+        
+        private void Calc()
         {
-            int count = 0;
             foreach (var row in PuzzleInput)
             {
                 var s = row.Split(',');
-
                 var a = s[0].Split('-').Select(int.Parse).ToArray();
                 var b = s[1].Split('-').Select(int.Parse).ToArray();
 
-                if (part1 && (
-                    (a[0] <= b[0] && a[1] >= b[1]) || //a surrounds b
-                    (a[0] >= b[0] && a[1] <= b[1])    //b surrounds a
-                    ))
-                {
-                    count++;
-                }
-                else if (!part1 && (
-                    (a[0] <= b[0] && a[1] >= b[0]) || //a surrounds b[0]
-                    (a[0] <= b[1] && a[1] >= b[1]) || //a surrounds b[1]
-                    (b[0] <= a[0] && b[1] >= a[0]) || //b surrounds a[0]
-                    (b[0] <= a[1] && b[1] >= a[1])    //b surrounds a[1]
-                    ))
-                {
-                    count++;
-                }
+                if ((a[0] <= b[0] && a[1] >= b[1]) || //a surrounds b
+                    (a[0] >= b[0] && a[1] <= b[1]))   //b surrounds a
+                    _countA++;
+
+                if(!(a[1] < b[0] || b[1] < a[0] )) //not ( a completely precedes b
+                    _countB++;                     //    ||b completely precedes a)
             }
-            return count;
         }
     }
 }
