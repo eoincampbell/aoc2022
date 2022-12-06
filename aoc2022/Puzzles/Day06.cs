9152a892ca98d940f05242aa9dee200ba6fdfe6f
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-06 10:04:10 +0000

    Day 06 Cleanup

--- aoc2022/Puzzles/Day06.cs
+++ aoc2022/Puzzles/Day06.cs
@@ -1,32 +1,27 @@
 ﻿//AOC2022 - Day 06
 //Result 1: 1707
 //Result 2: 3697
-
+using MoreLinq;
 namespace aoc2022.Puzzles
 {
     internal class Day06 : Puzzle
     {
         public override int Day => 6;
-        public override string Name => "";
+        public override string Name => "Tuning Trouble";
         protected override object RunPart1() => Calc(4);
         protected override object RunPart2() => Calc(14);
         public Day06() : base("Inputs/Day06.txt") { }
 
         private object Calc(int len)
         {
-            var x = PuzzleInput.First();
-            string f(char _, int i) //chunker function
-                => x.Substring(i, (i + len > x.Length) ? x.Length - i : len);
-
             var i = 0;
-            foreach (var yy in x.Select(f))
+            foreach (var yy in PuzzleInput.First().Window(len).Select(s => s.Distinct().Count()))
             {
-                if (yy.ToCharArray().Distinct().Count() == len)
-                    return i + len;
+                if (yy == len) break;
                 i++;
             }
 
-            return -1;
+            return i+len;
         }
     }
 }
