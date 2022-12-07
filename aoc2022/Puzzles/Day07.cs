88b1f520d1120bd29a0d7bcdce1381b9f338e0ae
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-07 12:15:47 +0000

    Day 07 Cleanup

--- aoc2022/Puzzles/Day07.cs
+++ aoc2022/Puzzles/Day07.cs
@@ -7,13 +7,11 @@ namespace aoc2022.Puzzles
     {
         public override int Day => 7;
         public override string Name => "No Space Left On Device";
-        protected override object RunPart1() => Part1();
+        protected override object RunPart1() => GetSize(_root);
         protected override object RunPart2() => Part2();
         private readonly Dir _root;
         public Day07() : base("Inputs/Day07.txt") =>
             _root = LoadDirectoryStructure();
-
-        private object Part1() => GetSize(_root);
 
         private object Part2()
         {
@@ -30,8 +28,7 @@ namespace aoc2022.Puzzles
             if (srcSize < bestmatch.Size() && srcSize > shortfall)
                 bestmatch = src;
 
-            //recurse sub directories
-            foreach (var d in src.Directories)
+            foreach (var d in src.Dirs) //recurse sub directories
                 bestmatch = Find(d, bestmatch, shortfall);
 
             return bestmatch;
@@ -39,57 +36,41 @@ namespace aoc2022.Puzzles
 
         private long GetSize(Dir dir) =>
             ((dir.Size() < 100000) ? dir.Size() : 0) //current if over 100,000
-                + dir.Directories.Select(d => GetSize(d)).Sum(); //+ all subs
+                + dir.Dirs.Select(d => GetSize(d)).Sum(); //+ all subs
 
         private Dir LoadDirectoryStructure()
         {
-            var root = new Dir("/", null);
-            var cur = root;
-
+            Dir root = new("/", null!), cur = root;
             foreach (var l in PuzzleInput.Skip(1))
             {
-                if (l == "$ ls") { }//NO OP
-                else if (l == "$ cd ..")
+                switch (l)
                 {
-                    cur = cur.Parent;
-                }
-                else if (l.StartsWith("$ cd "))
-                {
-                    cur = cur.Directories.Single(d => d.Name == l[5..]);
-                }
-                else if (l.StartsWith("dir "))
-                {
-                    cur.Directories.Add(new Dir(l[4..], cur));
-                }
-                else //must be a file
-                {
-                    var f = l.Split(' ');
-                    cur.Files.Add(new File(f[1], long.Parse(f[0])));
+                    case "$ cd ..": 
+                        cur = cur.Parent;
+                        break;
+                    case "$ ls": break;
+                    case string x when x.StartsWith("dir "):
+                        cur.Dirs.Add(new Dir(l[4..], cur));
+                        break;
+                    case string x when x.StartsWith("$ cd "):
+                        cur = cur.Dirs.Single(d => d.Name == l[5..]);
+                        break;
+                    default:
+                        var f = l.Split(' ');
+                        cur.Files.Add(new File(f[1], long.Parse(f[0])));
+                        break;
                 }
             }
             return root;
         }
 
         private record File(string Name, long Size);
-
-        private class Dir
+        private record Dir(string Name, Dir Parent)
         {
-            public string Name { get; set; }
-            public Dir Parent { get; set; }
-            public IList<Dir> Directories { get; set; }
-            public IList<File> Files { get; set; }
-
-            public Dir(string name, Dir parent)
-            {
-                Name = name;
-                Parent = parent;
-                Directories = new List<Dir>();
-                Files = new List<File>();
-            }
-
-            public long Size() =>
-                Files.Select(f => f.Size).Sum() +
-                Directories.Select(d => d.Size()).Sum();
+            public IList<Dir> Dirs { get; set; } = new List<Dir>();
+            public IList<File> Files { get; set; } = new List<File>();
+            public long Size() => Files.Select(f => f.Size).Sum() +
+                Dirs.Select(d => d.Size()).Sum();
         }
     }
 }
