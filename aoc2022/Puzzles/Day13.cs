36cf5aa5833cc4329516bcdc8504c426a45fc607
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-13 14:47:44 +0000

    Day 13 Cleanup

--- aoc2022/Puzzles/Day13.cs
+++ aoc2022/Puzzles/Day13.cs
@@ -13,39 +13,29 @@ namespace aoc2022.Puzzles
         protected override object RunPart1() => Part1();
         protected override object RunPart2() => Part2();
         private readonly IEnumerable<Packet> _signals;
-        private Packet _s1 => Parse("[[2]]");
-        private Packet _s2 => Parse("[[6]]");
+        private Packet [] Ord => new Packet[] { Parse("[[2]]"), Parse("[[6]]") };
         public Day13() : base("Inputs/Day13.txt") =>
             _signals = PuzzleInput
                 .Where(s => !string.IsNullOrEmpty(s))
                 .Select(Parse);
 
-        private object Part2()
-        {
-           var list = _signals
-                .Append(_s1)
-                .Append(_s2)
-                .OrderBy(x => x, Comparer<Packet>.Create(Compare))
-                .ToList();
-
-            return (list.IndexOf(_s1) + 1) * (list.IndexOf(_s2) + 1);
-        }
-
+        private object Part2() =>
+            _signals.Concat(Ord)
+                 .OrderBy(x => x, Comparer<Packet>.Create(Compare))
+                 .Select((e, i) => e == Ord[0] || e == Ord[1] ? i + 1 : 1)
+                 .Product();
+          
         private object Part1() =>
-            PuzzleInput
-                .Where(x => !string.IsNullOrEmpty(x))
-                .Select(Parse)
-                .Batch(2)
-                .Select(x => x.ToArray())
+            _signals.Batch(2)
                 .Index()
-                .Select((kv, i) => (Compare(kv.Value[0], kv.Value[1]) < 0) ? i + 1 : 0)
+                .Select((e, i) => (Compare(e.Value.ElementAt(0), e.Value.ElementAt(1)) < 0) ? i + 1 : 0)
                 .Sum();
 
         private int Compare(Packet left, Packet right) =>
         (left, right) switch
         {
-            (Packet l, null)       => +1,
-            (null, Packet r)       => -1,
+            (Packet l, null) => +1,
+            (null, Packet r) => -1,
             (IPacket l, IPacket r) => Comparer<int>.Default.Compare(l.Value, r.Value),
             (IPacket l, APacket r) => Compare(new APacket(new Packet[] { l }), r),
             (APacket l, IPacket r) => Compare(l, new APacket(new Packet[] { r })),
@@ -63,9 +53,9 @@ namespace aoc2022.Puzzles
         private static Packet Parse(string value) =>
             Parse(JsonSerializer.Deserialize<JsonElement>(value));
 
-        private static Packet Parse(JsonElement element) =>
-           (element.ValueKind == JsonValueKind.Number)
-                ? new IPacket(element.GetInt32())
-                : new APacket(element.EnumerateArray().Select(Parse).ToArray());
+        private static Packet Parse(JsonElement e) =>
+           (e.ValueKind == JsonValueKind.Number)
+                ? new IPacket(e.GetInt32())
+                : new APacket(e.EnumerateArray().Select(Parse).ToArray());
     }
 }
