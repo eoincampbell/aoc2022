4b7855cc99d8420f31f28da551f043fd7fac658a
Eoin Campbell <eoin.campbell@channelsight.com>
2022-12-14 17:03:11 +0000

    Day 14 Cleanup

--- aoc2022/Puzzles/Day14.cs
+++ aoc2022/Puzzles/Day14.cs
@@ -14,23 +14,25 @@ namespace aoc2022.Puzzles
         protected override object RunPart2() => PourSand(false);
         private readonly Dictionary<Point, char> _map;
         private readonly int _bottomFloor;
+        private readonly int _bottomRock;
         private const char SAND = '.';
         private const char ROCK = '▓';
+        private static int MN(int a, int b) => Math.Min(a, b);
+        private static int MX(int a, int b) => Math.Max(a, b);
         public Day14() : base("Inputs/Day14.txt")
         {
             _map = new Dictionary<Point, char>();
             LoadMap();
-            _bottomFloor = _map.Max(kvp => kvp.Key.Y) + 2;
+            _bottomRock = _map.Max(k => k.Key.Y);
+            _bottomFloor = _bottomRock + 2;
         }
 
         private void PrintMap()
         {
-            int minx = _map.Min(kvp => kvp.Key.X),
-                maxx = _map.Max(kvp => kvp.Key.X),
-                miny = _map.Min(kvp => kvp.Key.Y),
-                maxy = _map.Max(kvp => kvp.Key.Y);
-
-            for(int y = MN(0, miny); y <= maxy; y++)
+            int minx = _map.Min(k => k.Key.X),
+                maxx = _map.Max(k => k.Key.X),
+                miny = _map.Min(k => k.Key.Y);
+            for (int y = MN(0, miny); y <= _bottomFloor; y++)
             {
                 for (int x = minx; x <= maxx; x++)
                     Console.Write(_map.ContainsKey(new Point(x, y)) ? _map[new Point(x, y)] : ' ');
@@ -39,42 +41,41 @@ namespace aoc2022.Puzzles
             }
         }
 
-        private static int MN(int a, int b) => Math.Min(a,b);
-        private static int MX(int a, int b) => Math.Max(a, b);
         private void LoadMap() =>
             PuzzleInput
                 .ToList()
                 .ForEach(l =>
                 {
                     l.Split(" -> ")
-                    .Select(s => s.Split(','))
-                    .Select(p => new Point(int.Parse(p[0]), int.Parse(p[1])))
-                    .Pairwise((a, b) => new { S = a, T = b })
+                    .Select(s => s.Split(',').Select(int.Parse).ToArray())
+                    .Pairwise((a, b) => new { S = new Point(a[0], a[1]), T = new Point(b[0], b[1]) })
                     .ToList()
-                    .ForEach(p =>
+                    .ForEach(f =>
                     {
-                        if (p.S.X == p.T.X)
-                            for (var y = MN(p.S.Y, p.T.Y); y <= MX(p.S.Y, p.T.Y); y++)
-                                _map.TryAdd(new Point(p.S.X, y), ROCK);
+                        if (f.S.X == f.T.X) //vertical rocks
+                        {
+                            for (var y = MN(f.S.Y, f.T.Y); y <= MX(f.S.Y, f.T.Y); y++)
+                                _map.TryAdd(new Point(f.S.X, y), ROCK);
+                        }
 
-                        if (p.S.Y == p.T.Y)
-                            for (var x = MN(p.S.X, p.T.X); x <= MX(p.S.X, p.T.X); x++)
-                                _map.TryAdd(new Point(x, p.S.Y), ROCK);
+                        if (f.S.Y == f.T.Y) //horizontal rocks
+                        {
+                            for (var x = MN(f.S.X, f.T.X); x <= MX(f.S.X, f.T.X); x++)
+                                _map.TryAdd(new Point(x, f.S.Y), ROCK);
+                        }
                     });
                 });
 
-        private object PourSand(bool part1)
+        private object PourSand(bool part1, bool stop = false)
         {
-            var bottomY = _map.Max(kvp => kvp.Key.Y);
-            var stop = false;
             while (!stop)
             {
                 var grain = new Point(500, 0);
                 if (_map.ContainsKey(grain)) break; //source blocked
                 while (true)
                 {
-                    var temp = MoveGrain(grain, part1); 
-                    if (temp.Y >= bottomY && part1) stop = true;
+                    var temp = MoveGrain(grain, part1);
+                    if (temp.Y >= _bottomRock && part1) stop = true;
                     if (temp == grain || stop) break; //sand at rest
                     grain = temp;
                 }
@@ -95,7 +96,6 @@ namespace aoc2022.Puzzles
             if (!_map.ContainsKey(dd) && dd.Y < _bottomFloor) return dd;
             if (!_map.ContainsKey(dl) && dl.Y < _bottomFloor) return dl;
             if (!_map.ContainsKey(dr) && dr.Y < _bottomFloor) return dr;
-
             _map.TryAdd(curr, SAND);
             return curr;
         }
